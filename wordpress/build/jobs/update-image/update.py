a8d83ed8c2714fda3c64aee5b2e6068ead1294d2
Jason Ross <jaross@redhat.com>
2021-05-11 13:55:32 -0500

    fix fstrings cause im stupid

--- wordpress/build/jobs/update-image/update.py
+++ wordpress/build/jobs/update-image/update.py
@@ -18,7 +18,7 @@ api = client.ApiClient()
 core = client.CoreV1Api(api)
 custom = client.CustomObjectsApi(api)
 latest_build = custom.get_namespaced_custom_object(
-    'image.openshift.io', 'v1', namespace, 'imagestreamtags', f"${image_name}:latest")
+    'image.openshift.io', 'v1', namespace, 'imagestreamtags', f"{image_name}:latest")
 
 plugins = [(key.split('.')[3], value) for key, value in latest_build.get('image').get('dockerImageMetadata').get(
     'ContainerConfig').get('Labels').items() if key.startswith('org.wordpress.plugin')]
@@ -26,7 +26,7 @@ themes = [(key.split('.')[3], value) for key, value in latest_build.get('image')
     'ContainerConfig').get('Labels').items() if key.startswith('org.wordpress.theme')]
 
 for plugin in plugins:
-    latest = requests.get(f"https://api.wordpress.org/plugins/info/1.0/${plugin[0]}.json").json().get('version')
+    latest = requests.get(f"https://api.wordpress.org/plugins/info/1.0/{plugin[0]}.json").json().get('version')
     if latest != plugin[1]:
         update[f"${plugin[0].upper()}_VERSION"] = latest
 
@@ -38,4 +38,4 @@ if update:
         f'{{"data":{{${json.dumps(update)}}}}}')
 
     requests.post(
-        f"https://api.homelab.csfreak.com:6443/apis/build.openshift.io/v1/namespaces/${namespace}/buildconfigs/${image_name}/webhooks/${os.environ.get('WebHookSecretKey')}/generic")
+        f"https://api.homelab.csfreak.com:6443/apis/build.openshift.io/v1/namespaces/{namespace}/buildconfigs/{image_name}/webhooks/{os.environ.get('WebHookSecretKey')}/generic")
