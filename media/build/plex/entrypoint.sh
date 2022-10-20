f05a64da4a73fab2d37e0ec07d3812cb1ae96064
Jason Ross <jaross@redhat.com>
2022-10-20 12:23:11 -0500

    Squashed commit of the following:
    commit 6c8f9a5bfce47239e7a24ce38e03e20b4da5cf5b
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 23:05:20 2022 -0500
    
        add user in dockerfile
    
    commit 5d8faad8bcc20c02c5734dbe8d96c86494fff2a9
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 23:01:52 2022 -0500
    
        add user in dockerfile
    
    commit bb1e0ea36b90b42476ebcbc6059ae7d85a95ea0a
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 22:07:30 2022 -0500
    
        entrypoint
    
    commit 6a94b08611f5643f8bed6cc3c8b1399aa0f2383b
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 20:27:52 2022 -0500
    
        fix quoting
    
    commit 40333f949edac4aff6da00478d689ed615d1ff80
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 20:16:52 2022 -0500
    
        entrypoint syntax
    
    commit 87fe4be09ac3b8f08aa040da2a0300474edd5502
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 19:33:21 2022 -0500
    
        entrypoint string escape
    
    commit 5c254f3533663b96692a38db2a5f3c06aa54a305
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 19:28:45 2022 -0500
    
        chmod +x
    
    commit 157edb73829e644a082c3286157a0d81ee5decd9
    Author: Jason Ross <jaross@redhat.com>
    Date:   Wed Oct 19 18:53:12 2022 -0500
    
        add plex build

--- media/build/plex/entrypoint.sh
+++ media/build/plex/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 export HOME="/usr/lib/plexmediaserver"
-export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="/config/Library/Application Support" \ 
-export PLEX_MEDIA_SERVER_HOME="/usr/lib/plexmediaserver" \
-export PLEX_MEDIA_SERVER_INFO_VENDOR="Red Hat Enterprise Linux" \
-export PLEX_MEDIA_SERVER_INFO_DEVICE="Openshift Container Platform 4" \
-export PLEX_MEDIA_SERVER_INFO_MODEL="Container" \
+export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="/config/Library/Application Support"
+export PLEX_MEDIA_SERVER_HOME="/usr/lib/plexmediaserver"
+export PLEX_MEDIA_SERVER_INFO_VENDOR="Red Hat Enterprise Linux"
+export PLEX_MEDIA_SERVER_INFO_DEVICE="Openshift Container Platform 4"
+export PLEX_MEDIA_SERVER_INFO_MODEL="Container"
 export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION="ubi8"
 
-if ! [ -d $PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR]; then
-  mkdir -p $PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR
+if [ ! -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" ] ; then
+  mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"
 fi
 
 exec $@
