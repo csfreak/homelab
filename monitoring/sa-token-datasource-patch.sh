4a56aa7bbf74392e5410f97aa050099b8d036cdd
csfreak <github@csfreak.com>
2022-12-21 00:14:01 -0600

    fix job script

--- monitoring/sa-token-datasource-patch.sh
+++ monitoring/sa-token-datasource-patch.sh
@@ -4,4 +4,4 @@ TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
 PATCH="[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureJsonData/httpHeaderValue1\", \"value\": \"Bearer ${TOKEN}\"}]"
 
 set +x
-oc patch grafanadatasources thanos -n $(cat /run/secrets/kubernetes.io/serviceaccount/namespace) --type json --patch "${PATCH}"
+oc patch grafanadatasources prometheus-grafanadatasource -n $(cat /run/secrets/kubernetes.io/serviceaccount/namespace) --type json --patch "${PATCH}"
